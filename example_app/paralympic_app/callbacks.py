d4a25970e25847e8442391799f6b4c8337ec0b35
paulferri2407 <92063627+paulferri2407@users.noreply.github.com>
2022-04-02 17:29:27 +0100

    Update callbacks.py

--- example_app/paralympic_app/callbacks.py
+++ example_app/paralympic_app/callbacks.py
@@ -2,7 +2,7 @@ import json
 
 from dash import Output, Input
 
-import paralympic_app.create_charts as cc
+import website.paralympic_app.create_charts as cc
 
 
 def register_callbacks(dash_app):
@@ -15,7 +15,6 @@ def register_callbacks(dash_app):
     def update_output_div(event_variable):
         """
         Call back for updating the line chart when the type of data to display is changed.
-
         :param event_variable: one of the following which are columns in the paralympics dataset ['EVENTS', 'SPORTS',
         'COUNTRIES', 'PARTICIPANTS']
         :return: plotly.px.Figure The line chart representing the chosen variable
@@ -31,7 +30,6 @@ def register_callbacks(dash_app):
     def show_hide_ratio_charts(selected_types):
         """
         Callback to display or hide the winter and summer male:female ratio bar charts depending on the checkbox values.
-
         :param selected_types: List of checkbox values for the event type(s) (Winter and/or Summer)
         :return: a list of values, the first sets visibility for the Winter chart, the second for the Summer
         """
@@ -50,7 +48,6 @@ def register_callbacks(dash_app):
     def display_hover_data(hoverData):
         """
         Callback to find the highlight text for a given paralympic event when it is hovered over on the map.
-
         :param hoverData: he `hoverData` from the Input
         :return: text for the paragraph with id='highlight-text'
         """
